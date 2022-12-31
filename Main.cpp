e0b95d3ebf6cdb3e08cbc8a3c49c601fed25999a
Muhamad Yazid Imani <imaniyazid@gmail.com>
2022-12-31 17:23:44 +0700

    create class user

--- Main.cpp
+++ Main.cpp
@@ -3,3 +3,33 @@
 #include <fstream>
 #include <stdlib.h>
 #include <iomanip>
+
+using namespace std;
+
+// Class User
+class User
+{
+private:
+    // deklarasi atribut dari class user
+    string username = "null";
+    string password = "null";
+    ofstream masukin;
+    ifstream nampilin;
+    string namafile = "data/dataUser.txt";
+
+public:
+    // constructor user
+    User(string username, string password);
+    // prototype
+    // method getter dan beberapa method lainnya
+    string getUser();
+    string getUsername();
+    bool daftar();
+    int login(string username, string password);
+};
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
